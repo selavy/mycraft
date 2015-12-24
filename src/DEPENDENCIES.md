735c45daf28d50912290eab9c6b10abf424adc42
Peter Lesslie <pclesslie@gmail.com>
2015-12-23 21:14:25 -0600

    First GLEW app

--- src/DEPENDENCIES.md
+++ src/DEPENDENCIES.md
@@ -1,2 +1,6 @@
 sudo apt-get install libgl1-mesa-glx
 sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libglew1.10 
+sudo apt-get install libglew-dev
+sudo apt-get install freeglut3 
+sudo apt-get install freeglut3-dev
--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
 
 int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("GLEW Test");
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        exit(0);
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
     return 0;
 }
